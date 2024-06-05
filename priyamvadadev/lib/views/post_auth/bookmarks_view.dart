065bf3a4eb46bbdfb54da24a31f0c83c5b4ba6b8
ItsAbhinavM <abhinav051017@gmail.com>
2024-06-05 18:28:22 +0530

    feat: add notes page

--- priyamvadadev/lib/views/post_auth/bookmarks_view.dart
+++ priyamvadadev/lib/views/post_auth/bookmarks_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Bookmarks extends StatelessWidget {
   const Bookmarks({super.key});
@@ -14,7 +15,7 @@ class Bookmarks extends StatelessWidget {
           children: [
             Text("Mahabharata",style: Theme.of(context).textTheme.headlineSmall?.copyWith(fontWeight: FontWeight.w500,fontSize: 18),),
             SizedBox(height: screenHieght*0.01,),
-            Text("Notes",style: Theme.of(context).textTheme.headlineSmall?.copyWith(fontSize: 14),)
+            Text("Notes",style: Theme.of(context).textTheme.headlineSmall?.copyWith(fontSize: 14,fontWeight: FontWeight.bold,color: Color(0xff6D6464)),)
           ],
         ),
         centerTitle: true,
@@ -25,14 +26,37 @@ class Bookmarks extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.only(left: screenWidth*0.10,right: screenWidth*0.10,top: screenWidth*0.10),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text("Chapter 1: ",style: Theme.of(context).textTheme.headlineSmall?.copyWith(fontWeight: FontWeight.w500,fontSize: 18),),
-                SizedBox(width: screenWidth*0.01,),
-                Text("Adiparva",style: Theme.of(context).textTheme.headlineSmall?.copyWith(fontWeight: FontWeight.w500,fontSize: 18),),
+              Expanded(
+                  child: Column(
+                    // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Chapter 1: ",style: Theme.of(context).textTheme.headlineSmall?.copyWith(fontWeight: FontWeight.w500,fontSize: 18),),
+                          SizedBox(width: screenWidth*0.01,),
+                          Flexible(child: Text("Adiparva ",style: Theme.of(context).textTheme.headlineSmall?.copyWith(fontWeight: FontWeight.w500,fontSize: 18),softWrap: true)),
+                        ],
+                      ),
+                      Text("Created on 8th August 2024 at 10:45 ",style: Theme.of(context).textTheme.headlineSmall,softWrap: true,overflow: TextOverflow.fade,)
+                    ],
+                  ),
+                ),
+                Icon(Icons.delete)
               ],
-            )
+            ),
+            SizedBox(height: screenHieght*.025,),
+            Text("Note at 05:29:36",style: Theme.of(context).textTheme.headlineSmall?.copyWith(fontWeight: FontWeight.w500,fontSize: 18)),
+            SizedBox(height: screenHieght*.0125,),
+            Text("How the Mahābhārata came to be narrated by Sauti to the assembled rishis at Naimisharanya, after having been recited at the sarpasattra of Janamejaya by Vaisampayana at Takshashila. The history and genealogy of the Bharata and Bhrigu races are recalled, as is the birth and early life of the Kuru princes (adi means first). Adi parva describes Pandava's birth, childhood, education, marriage, struggles due to conspiracy as well as glorious achievements.",style: Theme.of(context).textTheme.headlineSmall?.copyWith(fontWeight: FontWeight.w300,fontSize: 18))
           ],
         ),
       ),
